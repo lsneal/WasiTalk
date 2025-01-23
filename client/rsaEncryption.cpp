b5cf81ff8633af64b9f8e543bd4f799b53ece1fc
amnesia <amnesia@amnesia>
2025-01-23 13:21:51 +0000

    Load PEM key and read

--- client/rsaEncryption.cpp
+++ client/rsaEncryption.cpp
@@ -3,6 +3,25 @@
 void    Client::EncryptMessagesWithRSA() 
 {
 
-    
+    /*
+        Load PublicKey with PEM format
+        BIO *BIO_new_mem_buf(const void *buf, int len);
+    */
+    BIO* bio = BIO_new_mem_buf(this->_publicKey.c_str(), -1);
+    if (!bio) {
+        std::cerr << "Error: load PEM" << std::endl;
+        return ;
+    }
+
+    /*
+        Read PublicKey with object bio
+        EVP_PKEY *PEM_read_bio_PUBKEY(BIO *bp, EVP_PKEY **x,
+                                       pem_password_cb *cb, void *u);
+    */
+    EVP_PKEY* public_key = PEM_read_bio_PUBKEY(bio, NULL, NULL, NULL);
+    if (!public_key) {
+        std::cerr << "Error: read PEM" << std::endl;
+        return ;
+    }
 
 }
