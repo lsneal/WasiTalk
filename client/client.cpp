82aefc993576767788d62712511d34d861332b43
amnesia <post.xes42@passmail.net>
2025-03-19 13:12:40 +0100

    Delete part of the key sends

--- client/client.cpp
+++ client/client.cpp
@@ -81,7 +81,7 @@ void    SendMsg(SSL* _ssl)
     {
         std::getline(std::cin, user_input);
         std::lock_guard<std::mutex> lock(sendMutex);
-        std::string messageEncode = base64_stringEncode(user_input);
+        std::string messageEncode = user_input;//base64_stringEncode(user_input);
         SSL_write(_ssl, messageEncode.c_str(), messageEncode.length());
         OPENSSL_cleanse(&messageEncode[0], messageEncode.size());
     }
@@ -121,53 +121,11 @@ int    Client::StartCommunicationWithServer(std::vector<char> buffer)
     SSL_write(this->_ssl, user_input.c_str(), user_input.length());
 
     // Send publickey RSA
-    buffer.assign(buffer.size(), 0);
-    SSL_write(this->_ssl, this->_publicKey.c_str(), this->_publicKey.length() - 1);
+    /*buffer.assign(buffer.size(), 0);
+    SSL_write(this->_ssl, this->_publicKey.c_str(), this->_publicKey.length() - 1);*/
 
-    // Read server message pseudo list
-    buffer.assign(buffer.size(), 0);
-    std::cout << "List of user connected: \n" << std::endl;
-    bytes_read = SSL_read(this->_ssl, buffer.data(), buffer.size());
-
-    if (CheckBytesRead(bytes_read, buffer.data()) == false)
-        return -1;
-
-    // Enter pseudo list for communicate or send code for solo user
-    std::string serv = "Solo on server";
-    if (serv.compare(buffer.data()) != 0) 
-    {
-        // while read client list 
-        /*buffer.assign(buffer.size(), 0);
-        while (buffer.data())
-        {
-            bytes_read = SSL_read(this->_ssl, buffer.data(), buffer.size());
-            std::cout << buffer.data();
-        }*/
-
-        std::cout << "Enter pseudo for open session: " << std::endl;
-        std::getline(std::cin, user_input);
-        this->_pseudoSession = user_input;
-        SSL_write(this->_ssl, user_input.c_str(), user_input.length());
-    
-        // receive rsa key
-        bytes_read = SSL_read(this->_ssl, buffer.data(), buffer.size());
-        EncryptAndSendAES(this->_publicKey);
-
-        return 1;
-    }
-    SSL_write(this->_ssl, this->_publicKey.c_str(), this->_publicKey.length());
-    
-    // Read for receive aes key session
-    bytes_read = SSL_read(this->_ssl, buffer.data(), buffer.size());
-
-    this->_pseudoSession = extractPseudo(buffer.data());
-    this->_aes = extractAndDecodeBase64(buffer.data(), this->_pseudoSession);
-
-    bytes_read = SSL_read(this->_ssl, buffer.data(), buffer.size());
-    this->_iv = extractAndDecodeBase64(buffer.data(), this->_pseudoSession);
-
-    std::cout << "aes = " << "'" << this->_aes << "'" << std::endl;
-    std::cout << "iv = " << "'" << this->_iv << "'" << std::endl;
+    std::cout << "for send msg -->" << " <pseudo> <msg>" << std::endl;
+    std::cout << "listing user -->" << " list" << std::endl;
 
     return 1;
 }
@@ -203,8 +161,7 @@ void Client::CommunicateWithServer()
     // Convert pem private key on RSA object for multithreading
     EVP_PKEY* privateKey = loadPrivateKeyFromString(this->_privateKey);
 
-    // std::thread ReceivMsgThread1(ReceivMsg, this->_ssl, privatekey, this->_aes, this->_iv);
-
+    // for send msg --> <username> <msg>
     std::thread ReceivMsgThread1(ReceivMsg, this->_ssl, this->_pseudoSession);
     std::thread SendMsgThread1(SendMsg, this->_ssl);
 
